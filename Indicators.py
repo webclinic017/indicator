39f15bbd59d59079470db77a3b108f4467511aa7
forvest <forvest.io@gmail.com>
2023-01-23 14:07:02 +0330

    Add IndicatorNeedlePointer function
    IndicatorNeedlePointer prepares AltcoinBTC numbers to showing on the Forvest website

--- Indicators.py
+++ Indicators.py
@@ -28,10 +28,60 @@ def AltcoinBTC(symbol1='ETHUSDT', symbol2='BTCUSDT'):
     mean3 = np.mean(rate1_2_3)
     sd3 = np.sqrt(np.var(rate1_2_3))
     normalized3 = [(_-mean3)/sd3 for _ in rate1_2_3]
-    result = { 'l_inteval':min(normalized1+normalized2+normalized3),
+    result = { 'l_interval':min(normalized1+normalized2+normalized3),
             'U_interval': max(normalized1+normalized2+normalized3),
             'state_6h':normalized1[-1],
             'state_1D':normalized2[-1],
             'state_6D':normalized3[-1],
             }
     return result
+
+
+def IndicatorNeedlePointer(symbol1='ETHUSDT', symbol2='BTCUSDT'):
+        altcoinBTC = AltcoinBTC(symbol1='ETHUSDT', symbol2='BTCUSDT')
+        l_interval = altcoinBTC['l_interval']
+        U_interval = altcoinBTC['U_interval']
+        state_6h = altcoinBTC['state_6h']
+        state_1D = altcoinBTC['state_1D']
+        state_6D = altcoinBTC['state_6D']
+
+        scaler = round(abs(l_interval-U_interval)/8)/10
+        if max(abs(l_interval),abs(U_interval))==abs(l_interval):
+                edited_U_interval = round(l_interval+(8*scaler*10))
+                edited_l_interval = round(l_interval)
+        else:
+                edited_l_interval = round(U_interval-(8*scaler*10))
+                edited_U_interval = round(U_interval)
+        
+        str_6h = str(state_6h)
+        pointindex_6h = str_6h.index('.')
+        if int(str_6h[pointindex_6h+1])%2==0:
+                edited_state_6h = float(str_6h[0:pointindex_6h+2])
+        else:
+                str_6h[pointindex_6h+1] = int(str_6h[pointindex_6h+1])+1
+                edited_state_6h = float(str_6h[0:pointindex_6h+2])
+        
+        str_1D = list(str(state_1D))
+        pointindex_1D = str_1D.index('.')
+        if int(str_1D[pointindex_1D+1])%2==0:
+                edited_state_1D = float(''.join(str_1D[0:pointindex_1D+2]))
+        else:
+                str_1D[pointindex_1D+1] = str(int(str_1D[pointindex_1D+1])+1)
+                edited_state_1D = float(''.join(str_1D[0:pointindex_1D+2]))
+        
+        str_6D = list(str(state_6D))
+        pointindex_6D = str_6D.index('.')
+        if int(str_6D[pointindex_6D+1])%2==0:
+                edited_state_6D = float(''.join(str_6D[0:pointindex_6D+2]))
+        else:
+                str_6D[pointindex_6D+1] = str(int(str_6D[pointindex_6D+1])+1)
+                edited_state_6D = float(''.join(str_6D[0:pointindex_6D+2]))
+
+        result = { 'l_interval':edited_l_interval,
+            'U_interval': edited_U_interval,
+            'state_6h':edited_state_6h,
+            'state_1D':edited_state_1D,
+            'state_6D':edited_state_6D,
+            'scaler':scaler
+            }
+        return result
